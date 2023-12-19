593036b37bf5a32a90bb711a0e1ee0cb42e3bba4
Kursad Cengiz Yildirim <k.cengizyildirim@gmail.com>
2023-12-20 00:46:44 +0300

    Created Java entity

---
+++ src/main/java/com/workintech/fruits/fswebs18d2/entity/Fruit.java
@@ -0,0 +1,27 @@
+package com.workintech.fruits.fswebs18d2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "fruit" , schema = "fsweb")
+public class Fruit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @Enumerated(EnumType.STRING)
+    private FruitType fruitType;
+}
